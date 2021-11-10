c47a2f1653e7bbbefa8d40f84d7a5ce7aebed121
fmpereira2309 <fmpereira2309@gmail.com>
2021-11-10 19:37:15 -0300

    Terceira alteração

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log('Vamos que vamos!')
 console.log('Vamos que vamos!')
 console.log('Vamos que vamos!')
+console.log('Vamos que vamos!')
+console.log('Vamos que vamos!')
+console.log('Vamos que vamos!')
