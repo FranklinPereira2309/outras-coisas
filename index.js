3388301e368e9a3f16876a77bbb0daf1857240d0
fmpereira2309 <fmpereira2309@gmail.com>
2021-11-10 19:39:05 -0300

    Primeiro alterado no ALTERADO

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 console.log('Vamos que vamos!')
 console.log('Vamos que vamos!')
 console.log('Vamos que vamos!')
-console.log('Vamos que vamos!')
-console.log('Vamos que vamos!')
-console.log('Vamos que vamos!')
